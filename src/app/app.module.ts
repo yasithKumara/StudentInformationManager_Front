fc6ea5e537b5ec307003b7322705ecb043f38e85
“Yasith <48174322+yasithKumara@users.noreply.github.com>
2021-04-12 22:36:15 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
+// import { HttpClientModule } from '@angular/common/http';
+// import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { StudentService } from './student.service';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     HttpClientModule,
+//     FormsModule
+//   ],
+//   providers: [StudentService],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentService } from './student.service';
+import { HttpClientModule } from '@angular/common/http';import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [StudentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
